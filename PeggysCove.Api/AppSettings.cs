ab5f2cfa1bf34379f6844db17bc1940b25b03b88
Andrei Mikhalevich <andrei.mikhalevich@gmail.com>
2021-08-10 00:12:49 +0300

    minor

--- PeggysCove.Api/AppSettings.cs
+++ PeggysCove.Api/AppSettings.cs
@@ -3,6 +3,5 @@ namespace PeggysCove.Api
     /// <summary>
     /// App configuration
     /// </summary>
-    /// <param name="AppName">App name</param>
     public record AppSettings(string AppName);
 }
