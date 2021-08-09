73aa8b0eb5bbf82db0b442c75b90b1c9cea54947
Andrei Mikhalevich <andrei.mikhalevich@gmail.com>
2021-08-09 15:17:49 +0300

    Halifax test project. Part 2

--- PeggysCove.Api/Program.cs
+++ PeggysCove.Api/Program.cs
@@ -1,23 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using PeggysCove.Api;
 
-namespace PeggysCove.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
-}
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+    .Build()
+    .Run();
