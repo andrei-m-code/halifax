6c2a81cedb938d000b66a260cba2b2ba74534be5
Andrei Mikhalevich <andrei.mikhalevich@gmail.com>
2021-08-16 17:22:56 +0300

    Json helper comments

--- Halifax.Core/Helpers/Json.cs
+++ Halifax.Core/Helpers/Json.cs
@@ -4,25 +4,45 @@ using Newtonsoft.Json.Serialization;
 
 namespace Halifax.Core.Helpers
 {
+    /// <summary>
+    /// Serialize or deserialize objects to and from JSON
+    /// </summary>
     public static class Json
     {
         // TODO: We need to allow customizations to this later on.
         private static readonly JsonSerializerSettings settings = GetDefaultSettings();
         
-        public static string Serialize<TObject>(TObject obj, Formatting formatting = Formatting.None) where TObject : class
+        /// <summary>
+        /// Serialize object to JSON string
+        /// </summary>
+        /// <param name="obj">Object to serialize</param>
+        /// <param name="formatting">Formatting (Indented or not)</param>
+        /// <typeparam name="TObject">Object type</typeparam>
+        /// <returns>JSON representation of an object</returns>
+        public static string Encode<TObject>(TObject obj, Formatting formatting = Formatting.None) where TObject : class
         {
             return obj != null 
                 ? JsonConvert.SerializeObject(obj, formatting, settings) 
                 : null;
         }
 
-        public static TObject Deserialize<TObject>(string objString) where TObject : class
+        /// <summary>
+        /// Deserialize object from JSON
+        /// </summary>
+        /// <param name="jsonString">JSON string representation of an object</param>
+        /// <typeparam name="TObject">Object type</typeparam>
+        /// <returns>An object that was serialized to JSON</returns>
+        public static TObject Decode<TObject>(string jsonString) where TObject : class
         {
-            return objString != null 
-                ? JsonConvert.DeserializeObject<TObject>(objString, settings) 
+            return jsonString != null 
+                ? JsonConvert.DeserializeObject<TObject>(jsonString, settings) 
                 : null;
         }
 
+        /// <summary>
+        /// Get default Halifax json serializer settings
+        /// </summary>
+        /// <returns>Json serializer settings object</returns>
         public static JsonSerializerSettings GetDefaultSettings()
         {
             var result = new JsonSerializerSettings();
