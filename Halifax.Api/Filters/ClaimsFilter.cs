18061bd76a788c50831ee4cc3164c292712a4dc3
slav6897337 <70911214+slav6897337@users.noreply.github.com>
2021-09-06 12:29:23 +0300

    Authorization

---
+++ Halifax.Api/Filters/ClaimsFilter.cs
@@ -0,0 +1,23 @@
+﻿using Halifax.Core.Exceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Halifax.Api.Filters
+{
+    public abstract class ClaimsFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var claims = context.HttpContext.User?.Claims?.ToList();
+
+            if (claims == null || !IsAuthorize(claims))
+            {
+                throw new HalifaxUnauthorizedException("Request is unauthorized");
+            }
+        }
+
+        protected abstract bool IsAuthorize(List<Claim> claims);
+    }
+}
