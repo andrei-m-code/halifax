3452ac74c6a6f87da39a5ff30dfaace3a3288e8b
Andrei Mikhalevich <andrei.mikhalevich@gmail.com>
2021-10-20 10:25:03 -0400

    crypto fixes

--- Halifax.Core/Helpers/Crypto.cs
+++ Halifax.Core/Helpers/Crypto.cs
@@ -11,36 +11,44 @@ namespace Halifax.Core.Helpers
 
         public static string Encrypt(string secret, string text)
         {
-            using var encryptor = Aes.Create();
-            using var ms = new MemoryStream();
-
             var clearBytes = Encoding.Unicode.GetBytes(text);
-            var pdb = new Rfc2898DeriveBytes(secret, salt);
+            using (var encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(secret, salt);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                    }
 
-            encryptor.Key = pdb.GetBytes(32);
-            encryptor.IV = pdb.GetBytes(16);
-
-            using var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write);
-            cs.Write(clearBytes, 0, clearBytes.Length);
-
-            return Convert.ToBase64String(ms.ToArray());
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
         }
 
         public static string Decrypt(string secret, string encrypted)
         {
-            using var encryptor = Aes.Create();
-            using var pdb = new Rfc2898DeriveBytes(secret, salt);
-            using var ms = new MemoryStream();
+            var cipherBytes = Convert.FromBase64String(encrypted);
 
-            encryptor.Key = pdb.GetBytes(32);
-            encryptor.IV = pdb.GetBytes(16);
-            using (var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+            using (var encryptor = Aes.Create())
             {
-                var cipherBytes = Convert.FromBase64String(encrypted);
-                cs.Write(cipherBytes, 0, cipherBytes.Length);
-            } // DO NOT remove brackets here!!! It cuts decoded string to 300 symbols. Why?
+                var pdb = new Rfc2898DeriveBytes(secret, salt);
 
-            return Encoding.Unicode.GetString(ms.ToArray());
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                    }
+
+                    return Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
         }
 
         public static bool TryDecrypt(string secret, string encrypted, out string result)
