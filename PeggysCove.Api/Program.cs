e29ebcc04620a2a66bb45774539f7708d4935e03
Andrei Mikhalevich <andrei.mikhalevich@gmail.com>
2021-11-09 11:09:05 -0500

    halifax .net6.0 update

--- PeggysCove.Api/Program.cs
+++ PeggysCove.Api/Program.cs
@@ -1,7 +1,17 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Halifax.Api;
+using Halifax.Core;
+using Halifax.Core.Helpers;
 using PeggysCove.Api;
 
-Host.CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-    .Build().Run();
+var secret = "Test JWT Token (at least 16 chars)";
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHalifax(builder => builder
+    .SetName(Env.GetSection<AppSettings>().AppName)
+    .ConfigureAuthentication(secret, false, false, false));
+
+var jwt = Jwt.Create(secret, new List<Claim>(), DateTime.UtcNow.AddYears(1));
+L.Info(jwt);
+
+var app = builder.Build();
+app.UseHalifax();
