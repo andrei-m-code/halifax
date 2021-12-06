44527dbfc8abb165a76774da896f04166a027f78
Andrei Mikhalevich <andrei.mikhalevich@gmail.com>
2021-12-06 11:48:39 -0500

    minor

--- Halifax.Models/Timeframe.cs
+++ Halifax.Models/Timeframe.cs
@@ -1,19 +1,18 @@
-namespace Halifax.Models
+namespace Halifax.Models;
+
+/// <summary>
+/// Open time interval
+/// </summary>
+public record Timeframe
 {
-    /// <summary>
-    /// Open time interval
-    /// </summary>
-    public record Timeframe
+    public Timeframe()
     {
-        public Timeframe()
-        {
-        }
-
-        public Timeframe(DateTime? from, DateTime? to)
-        {
-        }
-
-        public DateTime? From { get; set; }
-        public DateTime? To { get; set; }
     }
+
+    public Timeframe(DateTime? from, DateTime? to)
+    {
+    }
+
+    public DateTime? From { get; set; }
+    public DateTime? To { get; set; }
 }
