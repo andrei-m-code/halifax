bfd42ee0649e0abcf79e26a478eb407dca12bd92
Andrei <andrei.mikhalevich@gmail.com>
2021-12-28 23:21:33 +0300

    Request Rewind and Error request logging

--- Halifax.Api/Controllers/ErrorsController.cs
+++ Halifax.Api/Controllers/ErrorsController.cs
@@ -19,12 +19,37 @@ public class ErrorsController : ControllerBase
     [Route("error")]
     public async Task<object> Error()
     {
-        var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+        //var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+        var context = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
         var exception = context?.Error;
+
+        await LogRequestAsync(context);
+
         var (response, code) = await exceptionHandler.HandleAsync(exception);
 
         Response.StatusCode = (int)code;
 
         return response;
+    }
+
+    private async Task LogRequestAsync(IExceptionHandlerPathFeature context)
+    {
+        var path = context.Path;
+        var body = Request.Body;
+        var bodyString = string.Empty;
+
+        if (Request.QueryString.HasValue)
+        {
+            path += Request.QueryString.Value;
+        }
+
+        if (body.CanRead)
+        {
+            body.Position = 0;
+            using var stream = new StreamReader(Request.Body);
+            bodyString = await stream.ReadToEndAsync();
+        }
+
+        L.Info($"Error happened when processing the request \r\n{Request.Method}: {path}\r\nBODY: {bodyString}");
     }
 }
