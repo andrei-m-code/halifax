f749bcb6ed65a8b4b660535ca81787ebea3b789d
Andrei <andrei.mikhalevich@gmail.com>
2022-05-30 06:59:52 -0400

    NewIfEmpty comments

--- Halifax.Core/Extensions/GuidExtensions.cs
+++ Halifax.Core/Extensions/GuidExtensions.cs
@@ -2,6 +2,11 @@ namespace Halifax.Core.Extensions;
 
 public static class GuidExtensions
 {
+    /// <summary>
+    /// Returns new Guid if passed in Guid is null or empty
+    /// </summary>
+    /// <param name="guid">Guid in question</param>
+    /// <returns>Guid or new Guid</returns>
     public static Guid NewIfEmpty(this Guid? guid)
     {
         return guid == Guid.Empty || !guid.HasValue
