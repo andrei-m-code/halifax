3ec00133b4ce3b5aedb56f63a53eff1c96061bab
Andrei <andrei.mikhalevich@gmail.com>
2022-06-10 02:10:38 -0400

    Halifax client extensions for quick registration

---
+++ Halifax.Http/HalifaxHttpClientExtensions.cs
@@ -0,0 +1,65 @@
+using System.Net.Http.Headers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Halifax.Http;
+
+/// <summary>
+/// Halifax ServiceCollection AddHalifaxHttpClient extensions for easy HTTP client registration
+/// </summary>
+public static class HalifaxHttpClientExtensions
+{
+    public static IServiceCollection AddHalifaxHttpClient<THalifaxHttpClient>(
+        this IServiceCollection services, 
+        string defaultBaseUrl, 
+        Action<IServiceProvider, HttpClient> configure = null) where THalifaxHttpClient : HalifaxHttpClient
+    {
+        return services.AddHalifaxHttpClient<THalifaxHttpClient>(
+            defaultBaseUrl, 
+            null, 
+            (provider, client) => configure?.Invoke(provider, client));
+    }
+    
+    public static IServiceCollection AddHalifaxHttpClient<THalifaxHttpClient>(
+        this IServiceCollection services, 
+        string defaultBaseUrl, 
+        Action<HttpClient> configure = null) where THalifaxHttpClient : HalifaxHttpClient
+    {
+        return services.AddHalifaxHttpClient<THalifaxHttpClient>(
+            defaultBaseUrl, 
+            null, 
+            (_, client) => configure?.Invoke(client));
+    }
+    
+    public static IServiceCollection AddHalifaxHttpClient<THalifaxHttpClient>(
+        this IServiceCollection services, 
+        string defaultBaseUrl, 
+        string defaultBearerToken, 
+        Action<HttpClient> configure = null) where THalifaxHttpClient : HalifaxHttpClient
+    {
+        return services.AddHalifaxHttpClient<THalifaxHttpClient>(
+            defaultBaseUrl, 
+            defaultBearerToken, 
+            (_, client) => configure?.Invoke(client));
+    }
+    
+    public static IServiceCollection AddHalifaxHttpClient<THalifaxHttpClient>(
+        this IServiceCollection services, 
+        string defaultBaseUrl, 
+        string defaultBearerToken, 
+        Action<IServiceProvider, HttpClient> configure = null) where THalifaxHttpClient : HalifaxHttpClient
+    {
+        services.AddHttpClient<THalifaxHttpClient>((provider, client) =>
+        {
+            client.BaseAddress = new Uri(defaultBaseUrl);
+
+            if (!string.IsNullOrWhiteSpace(defaultBearerToken))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", defaultBearerToken);
+            }
+
+            configure?.Invoke(provider, client);
+        });
+
+        return services;
+    }
+}
