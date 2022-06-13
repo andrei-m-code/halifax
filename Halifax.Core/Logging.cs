8a030a9137fc33af8ae80cb190ec62a3ed96e6dc
Andrei <andrei.mikhalevich@gmail.com>
2022-06-13 05:22:09 -0400

    Logging improvements

--- Halifax.Core/Logging.cs
+++ Halifax.Core/Logging.cs
@@ -1,3 +1,4 @@
+using Destructurama;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -9,19 +10,48 @@ public static class L
     static L()
     {
         Log.Logger = new LoggerConfiguration()
+            .Destructure.ToMaximumStringLength(1000)
+            .Destructure.ToMaximumDepth(5)
+            .Destructure.UsingAttributes()
             .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
             .MinimumLevel.Override("System", LogEventLevel.Error)
             .Enrich.FromLogContext()
             .WriteTo.Console(theme: AnsiConsoleTheme.Literate)
+            //.WriteTo.Console(new JsonFormatter())
             .CreateLogger();
     }
 
-    public static void Info(string info) => Log.Information(info);
+    public static void Info<TPropertyValue>(string messageTemplate, TPropertyValue propertyValue) =>
+        Log.Information(messageTemplate, propertyValue);
+    
+    public static void Info(string messageTemplate, params object[] propertyValues) => 
+        Log.Information(messageTemplate, propertyValues);
 
+    public static void Warning(string messageTemplate, params object[] propertyValues) =>
+        Log.Warning(messageTemplate, propertyValues);
+    
+    public static void Warning(Exception exception, string messageTemplate, params object[] propertyValues) =>
+        Log.Warning(exception, messageTemplate, propertyValues);
+    
+    public static void Error(string messageTemplate, params object[] propertyValues) =>
+        Log.Error(messageTemplate, propertyValues);
+    
+    public static void Error(Exception exception, string messageTemplate, params object[] propertyValues) =>
+        Log.Error(exception, messageTemplate, propertyValues);
+
+    public static void Fatal(string messageTemplate, params object[] propertyValues) =>
+        Log.Error(messageTemplate, propertyValues);
+    
+    public static void Fatal(Exception exception, string messageTemplate, params object[] propertyValues) =>
+        Log.Error(exception, messageTemplate, propertyValues);
+    
+    [Obsolete]
     public static void Error(string message, Exception exception = null) => Log.Error(exception, message);
 
+    [Obsolete]
     public static void Warning(string message, Exception exception = null) => Log.Warning(exception, message);
 
+    [Obsolete]
     public static void Fatal(string message, Exception exception) => Log.Fatal(exception, message);
 }
 
@@ -37,3 +67,5 @@ public static class ServicesExtensions
             .AddFilter("System", LogLevel.Error));
     }
 }
+
+public class NotLoggedAttribute : Destructurama.Attributed.NotLoggedAttribute {}
