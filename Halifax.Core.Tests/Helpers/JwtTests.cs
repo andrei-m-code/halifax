f3b6e76f3110e3dfd8f47e29ec2254276e642344
Andrei <andrei.mikhalevich@gmail.com>
2023-07-09 15:14:57 -0400

    Package updates

--- Halifax.Core.Tests/Helpers/JwtTests.cs
+++ Halifax.Core.Tests/Helpers/JwtTests.cs
@@ -5,34 +5,37 @@ namespace Halifax.Core.Tests.Helpers;
 
 public class JwtTests
 {
+    private const string secret = "JWT-Secret-123qweRTY123123ZZZXXXYYYCCC11122233344455";
+    private readonly List<Claim> claims = new()
+    {
+        new("Test1", "Hello"),
+        new("Test2", "World")
+    };
+
     [Test]
     public void CreateAndReadJwt()
     {
-        var secret = "123qwe!@#,./*();zxcZXC000";
         var expiration = DateTimeOffset.Now.AddYears(1);
-        var expectedExpiration = expiration.ToUnixTimeSeconds();
-        var claimKey1 = "Test1";
-        var claimKey2 = "Test2";
-        var claimValue1 = "Hello";
-        var claimValue2 = "World";
-        var claims = new List<Claim>
-        {
-            new(claimKey1, claimValue1),
-            new(claimKey2, claimValue2)
-        };
-
         var createdJwt = Jwt.Create(secret, claims, expiration.DateTime);
-        var readClaims = Jwt.Read(secret, createdJwt);
+        var actualClaims = Jwt.Read(secret, createdJwt);
+        Assert.That(actualClaims, Is.Not.Null);
+        VerifyClaims(claims, actualClaims, expiration);
+    }
 
-        var test1 = readClaims.FirstOrDefault(c => c.Type == claimKey1);
-        var test2 = readClaims.FirstOrDefault(c => c.Type == claimKey2);
-        var expirationClaim = readClaims.FirstOrDefault(c => c.Type == "exp");
-        var expirationValue = Convert.ToInt64(expirationClaim?.Value);
-        
-        Assert.That(test1, Is.Not.Null);
-        Assert.That(test2, Is.Not.Null);
-        Assert.That(test1?.Value, Is.EqualTo(claimValue1));
-        Assert.That(test2?.Value, Is.EqualTo(claimValue2));
-        Assert.That(expirationValue, Is.EqualTo(expectedExpiration));
+    [Test]
+    public void ReadExistingJwt()
+    {
+        var jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJUZXN0MSI6IkhlbGxvIiwiVGVzdDIiOiJXb3JsZCIsIm5iZiI6MTY4ODkyOTUzNywiZXhwIjoxNjg4OTQzOTQyLCJpYXQiOjE2ODg5Mjk1Mzd9.o7svk12AG8SYoTdLM3HhOa0d8nXjWHumIrtRTaWoVcc";
+        var jwtExpiration = DateTimeOffset.FromUnixTimeSeconds(1688943942); 
+        var readClaims = Jwt.Read(secret, jwt, validateExpiration: false);
+        VerifyClaims(claims, readClaims, jwtExpiration);
+    }
+
+    private void VerifyClaims(List<Claim> expected, List<Claim> actual, DateTimeOffset expiration)
+    {
+        var anyMissing = expected.Any(exp => actual.FirstOrDefault(a => a.Type == exp.Type)?.Value != exp.Value);
+        var expirationMatching = actual.First(a => a.Type == "exp").Value == expiration.ToUnixTimeSeconds().ToString();
+        Assert.That(expirationMatching, Is.True);
+        Assert.That(anyMissing, Is.False);
     }
 }
