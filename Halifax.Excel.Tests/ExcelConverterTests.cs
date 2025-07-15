2ef282f83ad36b1d36893bbe328a9925bc9078d1
Andrei Mikhalevich <andrei.mikhalevich@gmail.com>
2025-07-15 19:24:38 -0400

    Excel improvements

---
+++ Halifax.Excel.Tests/ExcelConverterTests.cs
@@ -0,0 +1,178 @@
+﻿using CsvHelper.Configuration;
+
+namespace Halifax.Excel.Tests;
+
+public interface IPerson
+{
+    public string Name { get; }
+    public int Age { get; }
+    public string Company { get; }
+}
+
+class PersonClass : IPerson
+{
+    public PersonClass()
+    {
+    }
+    
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Company { get; set; }
+}
+
+record PersonRecord : IPerson
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Company { get; set; }
+}
+
+record PersonRecordConstructorOnly(string Name, int Age, string Company) : IPerson;
+
+public class ExcelConverterTests
+{
+    private static readonly string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+    private static readonly string filenameExcel = Path.Combine(desktop, "Export.xlsx");
+    private static readonly string filenameCsv = Path.Combine(desktop, "Export.csv");
+
+    [SetUp]
+    [TearDown]
+    public void Setup()
+    {
+        if (File.Exists(filenameExcel)) File.Delete(filenameExcel);
+        if (File.Exists(filenameCsv)) File.Delete(filenameCsv);
+    }
+    
+    [Test]
+    public async Task ReadWriteCsv_Class()
+    {
+        var converter = new ExcelConverter<PersonClass>();
+        converter.AddMapping("Full Name", p => p.Name);
+        converter.AddMapping("Age", p => p.Age);
+        List<PersonClass> people =
+        [
+            new() { Name = "John Smith", Age = 34, Company = "ABC Co" },
+            new() { Name = "John Doe", Age = 40, Company = "XYZ Company" }
+        ];
+        await ReadWriteCsvAsync(converter, people);
+    }
+    
+    [Test]
+    public async Task ReadWriteCsv_Record()
+    {
+        var converter = new ExcelConverter<PersonRecord>();
+        converter.AddMapping("Full Name", p => p.Name);
+        converter.AddMapping("Age", p => p.Age);
+        List<PersonRecord> people =
+        [
+            new() { Name = "John Smith", Age = 34, Company = "ABC Co" },
+            new() { Name = "John Doe", Age = 40, Company = "XYZ Company" }
+        ];
+        await ReadWriteCsvAsync(converter, people);
+    }
+
+    /// <summary>
+    /// This is not supported by CsvHelper yet, we're waiting for them to
+    /// start supporting it and we can uncomment it then.
+    /// </summary>
+    // [Test]
+    // public async Task ReadWriteCsv_Record_ConstructorOnly()
+    // {
+    //     var converter = new ExcelConverter<PersonRecordConstructorOnly>();
+    //     converter.AddMapping("Full Name", p => p.Name);
+    //     converter.AddMapping("Age", p => p.Age);
+    //     List<PersonRecordConstructorOnly> people =
+    //     [
+    //         new("John Smith", 34, "ABC Co"),
+    //         new("John Doe", 40, "XYZ Company")
+    //     ];
+    // }
+    
+    [Test]
+    public async Task ReadWriteExcel_Class()
+    {
+        var converter = new ExcelConverter<PersonClass>();
+        converter.AddMapping("Full Name", p => p.Name);
+        converter.AddMapping("Age", p => p.Age);
+        List<PersonClass> people =
+        [
+            new() { Name = "John Smith", Age = 34, Company = "ABC Co" },
+            new() { Name = "John Doe", Age = 40, Company = "XYZ Company" }
+        ];
+        await ReadWriteExcelAsync(converter, people);
+    }
+    
+    [Test]
+    public async Task ReadWriteExcel_Record()
+    {
+        var converter = new ExcelConverter<PersonRecord>();
+        converter.AddMapping("Full Name", p => p.Name);
+        converter.AddMapping("Age", p => p.Age);
+        List<PersonRecord> people =
+        [
+            new() { Name = "John Smith", Age = 34, Company = "ABC Co" },
+            new() { Name = "John Doe", Age = 40, Company = "XYZ Company" }
+        ];
+        await ReadWriteExcelAsync(converter, people);
+    }
+    
+    [Test]
+    public async Task ReadWriteExcel_Record_ConstructorOnly()
+    {
+        var converter = new ExcelConverter<PersonRecordConstructorOnly>();
+        converter.AddMapping("Full Name", p => p.Name);
+        converter.AddMapping("Age", p => p.Age);
+        List<PersonRecordConstructorOnly> people =
+        [
+            new("John Smith", 34, "ABC Co"),
+            new("John Doe", 40, "XYZ Company")
+        ];
+        await ReadWriteExcelAsync(converter, people);
+    }
+
+    private static async Task ReadWriteExcelAsync<TObject>(
+        ExcelConverter<TObject> converter, 
+        List<TObject> source) where TObject : IPerson
+    {
+        using var stream = new MemoryStream();
+        await converter.WriteExcelAsync(stream, source);
+        await File.WriteAllBytesAsync(filenameExcel, stream.ToArray());
+        await using var readStream = File.OpenRead(filenameExcel);
+        var records = converter.ReadExcel(readStream);
+        CompareLists(source, records);
+    }
+
+    private static async Task ReadWriteCsvAsync<TObject>(
+        ExcelConverter<TObject> converter, 
+        List<TObject> source) where TObject : IPerson
+    {
+        using var stream = new MemoryStream();
+        await converter.WriteCsvAsync(stream, source);
+        await File.WriteAllBytesAsync(filenameCsv, stream.ToArray());
+        await using var readStream = File.OpenRead(filenameCsv);
+
+        var records = await converter.ReadCsvAsync(readStream);
+        CompareLists(source, records);
+    }
+    
+    private static ExcelConverter<TObject> CreateConverter<TObject>() where TObject : IPerson
+    {
+        var converter = new ExcelConverter<TObject>();
+        converter.AddMapping("Full Name", p => p.Name);
+        converter.AddMapping("Age", p => p.Age);
+        return converter;
+    }
+    
+    private static void CompareLists<TObject>(List<TObject> left, List<TObject> right) where TObject : IPerson
+    {
+        if (left.Count != right.Count) throw new Exception($"List count mismatch: left={left.Count}, right={right.Count}");
+
+        for (var i = 0; i < left.Count; i++)
+        {
+            var l = left[i];
+            var r = right[i];
+            if (l.Name != r.Name || l.Age != r.Age || l.Company != r.Company)
+                throw new Exception($"Difference at index {i}: Left({l.Name}, {l.Age}, {l.Company}) vs Right({r.Name}, {r.Age}, {r.Company})");
+        }
+    }
+}
